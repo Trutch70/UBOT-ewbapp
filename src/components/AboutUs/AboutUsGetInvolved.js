c6ccb0a3f9ecc2b192a5c65b08177278e683529b
Traczyk <96traczyklukasz@gmail.com>
2022-06-30 03:47:20 +0200

    start mobile contact page

--- src/components/AboutUs/AboutUsGetInvolved.js
+++ src/components/AboutUs/AboutUsGetInvolved.js
@@ -7,7 +7,7 @@ const AboutUsGetInvolved = () => {
     return (
         <AboutUsSection className={classes['get-involved']}>
             <div className={classes['get-involved-header']}>
-                <img src={getInvolved} alt={"get involved!"}/>
+                <img src={getInvolved} alt={"get involved in helping ukraine during and after the war!"}/>
             </div>
             <div className={classes.paragraph}>
                 We want to reach as many people as possible who could help businesses operating in war-torn Ukraine. Each share on social media is a greater range of action and a chance for support. Let's help the Ukrainian economy survive. Putin will be defeated soon. And then we'll need all of these little businesses like never before! <strong>Together we can help them.</strong>
