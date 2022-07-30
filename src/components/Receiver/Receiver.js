3813117080d877ca94cf62feb90d6ab1a534d3a6
Traczyk <96traczyklukasz@gmail.com>
2022-07-30 16:22:22 +0200

    receiver images carousel

--- src/components/Receiver/Receiver.js
+++ src/components/Receiver/Receiver.js
@@ -5,21 +5,20 @@ import ContentContainer from '../Common/ContentContainer';
 import classes from './Receiver.module.css';
 import defaultImage from '../../assets/default-image.png';
 import ReceiverDescription from './ReceiverDescription';
+import Slider from "react-slick";
+import AwesomeImage from '../MainPage/AwesomeImages/AwesomeImage';
 
 const Receiver = () => {
     const [receiver, setReceiver] = useState({});
+    const [loaded, setLoaded] = useState(false);
     const [error, setError] = useState(null);
     const { receiverId } = useParams();
     const receiversProvider = useReceiversProvider();
 
-    let image = defaultImage;
-    if (receiver && receiver.image_path) {
-        image = receiver.image_path;
-    }
-
     useEffect(() => {
         receiversProvider.fetchReceiver(receiverId)
             .then(data => setReceiver(data))
+            .then(data => setLoaded(true))
             .catch(error => {
                 switch(error.message) {
                     case 'NOTFOUND':
@@ -32,16 +31,56 @@ const Receiver = () => {
             })
     }, [receiverId, receiversProvider])
 
+    const sliderSettings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        pauseOnHover: true,
+        adaptiveHeight: true,
+    };
+
+    let imagesContent =
+        <div className={classes['receiver-images']}>
+            <img src={defaultImage} alt={`Ukraine business ${receiver.name} from ${receiver.location ? receiver.location.name : 'unknown'}`}/>
+        </div>
+
+    if (loaded) {
+        imagesContent =
+            <div className={classes['receiver-images']}>
+                <Slider {...sliderSettings}>
+                    {
+                        console.log(receiver)
+                    }
+                    {
+                        receiver.images.map((imagePath, index) => {
+                            console.log(index);
+                            return (
+                                <div key={`awesome_${index}`}>
+                                    <AwesomeImage background={imagePath} />
+                                </div>
+                            )
+                        })
+                    }
+                </Slider>
+            </div>
+        ;
+    }
+
     return (
         <div className={"page-content"}>
             {
-                !error && receiver &&
+                !error && loaded &&
                 <ContentContainer>
-                    <div className={classes.container}>
-                        <div className={classes['receiver-images']}>
-                            <img src={image} alt={`Ukraine ${receiver.name} from ${receiver.location ? receiver.location.name : 'unknown'}`}/>
+                    <div className={`receiver ${classes.container}`}>
+                        {
+                            imagesContent
+                        }
+                        <div className={classes['description-container']}>
+                            <ReceiverDescription receiver={receiver} />
                         </div>
-                        { receiver.id && <ReceiverDescription receiver={receiver} />}
                     </div>
                 </ContentContainer>
             }
