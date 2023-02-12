b720e0d4873b3530e3839c93face70c0fe750c37
Traczyk <96traczyklukasz@gmail.com>
2023-02-12 17:06:58 +0100

    static images

--- src/assets/images/POS/POSProvider.js
+++ src/assets/images/POS/POSProvider.js
@@ -1,8 +1,12 @@
 import Img1 from './1.jpg';
+import Img2 from './Stolen-Iland-62e5aa624ba16.jpg';
+import Img3 from './stolen-iland-bb-62e5aa5a5968f.jpg';
 
 export default {
     main: Img1,
     images: [
         Img1,
+        Img2,
+        Img3,
     ],
 }
--- src/assets/images/Stoneup/StoneupProvider.js
+++ src/assets/images/Stoneup/StoneupProvider.js
@@ -1,8 +1,16 @@
 import Img1 from './1.jpg';
+import Img2 from './IMG-2066-11zon-63221521d4ce8.jpg';
+import Img3 from './IMG-2170-11zon-632214c3a88dd.jpg';
+import Img4 from './IMG-2236-11zon-632214e1c1315.jpg';
+import Img5 from './IMG-3334-11zon-632214cf492db.jpg';
 
 export default {
     main: Img1,
     images: [
         Img1,
+        Img2,
+        Img3,
+        Img4,
+        Img5,
     ]
 }
