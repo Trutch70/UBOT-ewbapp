8476ad4658c6e7cb8b1215b16dabf6d85c358599
Traczyk <96traczyklukasz@gmail.com>
2023-02-18 17:39:09 +0100

    typo fix

--- src/assets/images/Bagllet/BaglletImagesProvider.js
+++ src/assets/images/Bagllet/BaglletImagesProvider.js
@@ -1,7 +1,7 @@
 import Img1 from "./1.jpg";
 import Img2 from "./b116-12-1-62e59d8f697be.jpg";
-import Img3 from "./B117-13-1-62e59ffadf32e.jpg.jpg";
-import Img4 from "./baglet-1-6306785f0342e.jpg.jpg";
+import Img3 from "./B117-13-1-62e59ffadf32e.jpg";
+import Img4 from "./baglet-1-6306785f0342e.jpg";
 
 const BaglletImagesProvider = {
     main: Img1,
